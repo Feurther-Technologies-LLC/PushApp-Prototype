281a759ea4e5ff9bcd7f60973b3a215ce0f46334
Jonathan Zhang <yufengzhang483@gmail.com>
2025-02-16 22:24:01 -0500

    Commit Hello TianMing

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from types_of_exercise import TypeOfExercise
 from playsound import playsound
 import threading  # 导入线程库
 
+# Hello TianMing
 
 def play_sound(sound_file: str) -> None:
     threading.Thread(target=playsound, args=(
